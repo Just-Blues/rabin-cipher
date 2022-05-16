a3714a65e147b7c10cab4373eab439a86328e801
Just-Blues <101468565+Just-Blues@users.noreply.github.com>
2022-05-16 20:41:43 +0200

    ebc is working

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ def decryption(ciphered_msg, p, q):
     # for p
     if p % 4 == 3:
         r = auxilliary.sqrt_p_3_mod_4(ciphered_msg, p)
+        #print("P mod 4, r= ", r)
     elif p % 8 == 5:
         r = auxilliary.sqrt_p_5_mod_8(ciphered_msg, p)
+        #print("P mod 8, r= ", r)
     # for q
     if q % 4 == 3:
         s = auxilliary.sqrt_p_3_mod_4(ciphered_msg, q)
+        #print("Q mod 4, s= ", s)
     elif q % 8 == 5:
         s = auxilliary.sqrt_p_5_mod_8(ciphered_msg, q)
+        #print("Q mod 8, s= ", s)
 
     gcd, c, d = auxilliary.egcd(p, q)
+    #print(gcd, c, d)
     x = (r * d * q + s * c * p) % n
     y = (r * d * q - s * c * p) % n
     lst = [x, n - x, y, n - y]
@@ -31,25 +36,26 @@ def decryption(ciphered_msg, p, q):
 
     i = 0
     aux = 0
-
+    plaintext = 0
     #Find the correct answer from lst
-    while aux != ciphered_msg and i < 4:
-        aux = lst[i]**2 % (n*p)
+    while i < 4:
+        aux = lst[i]**2 % (n*p) #n = p^2 * q ?????
+        if aux == ciphered_msg:
+            #print(i,aux, "==", ciphered_msg)
+            plaintext = lst[i]
         i += 1
     
-    i -= 1
-    plaintext = lst[i]
+    #plaintext = lst[i]
     #print(plaintext)
-
 
     return plaintext
 
+
 #Changes string to 32-bits blocks
-def str_to_int(plaintext):
+def str_to_int(plaintext,radix):
     result = []
     a = 0
     j = len(plaintext)
-    radix = 256
     #Padding
     if j % 4 != 0:
         plaintext += "\0"*(4 - j%4)
@@ -63,14 +69,14 @@ def str_to_int(plaintext):
     return result
 
 #Translates 32 bit integares into string of 4 chars
-def int_to_str(number):
+def int_to_str(number,radix):
     plaintext = ""
     for i in range(0, len(number)):
         #binary_str = bin(number[i])
         while number[i] != 0:
-            a = int(number[i] % 256)
+            a = int(number[i] % radix)
             plaintext += chr(a)
-            number[i] = (number[i] - a) / 256
+            number[i] = (number[i] - a) / radix
 
     return plaintext
 
@@ -95,10 +101,9 @@ def cbc_encryption(plaintext, n ,initializing_vector):
     encrypted = []
     for i in range(0, len(plaintext)):
         if i == 0:
-            aux = plaintext[i] ^ initializing_vector
+            aux = plaintext[0] ^ initializing_vector
         else:
-            aux = encrypted[i-1] ^ plaintext[i]
-
+            aux = plaintext[i] ^ encrypted[i-1]  
         encrypted.append(encryption(aux,n))
 
     return encrypted
@@ -107,29 +112,66 @@ def cbc_decryption(ciphered_msg, p, q, initializing_vector):
     decrypted = []
     for i in range(0, len(ciphered_msg)):
         if i == 0:
-            aux = decryption(ciphered_msg[i],p,q) ^ initializing_vector
+            aux = decryption(ciphered_msg[0],p,q) ^ initializing_vector
         else:
             aux = decryption(ciphered_msg[i],p,q) ^ ciphered_msg[i-1]
 
         decrypted.append(aux)
 
-
-
     return decrypted
 
-text = "mdld68T 5l5 D0Xi1| 0hg"
-int_text = str_to_int(text)
+text = "podovne do obsydianu XD"
+radix = 256 #for bigger radix we need enormous key
+int_text = str_to_int(text, radix)
 print(int_text)
 
 #Some enormous keys are required for the decryption to work properly
-enc_test = ebc_encryption(int_text, 129096451502903) #43869243335189 = 32779^2 * 40829, 1338333791 = 32779 * 40829
-print(enc_test)
+key = 50539 * 50543 * 50539
+#key2 = 118249 * 118633
+#large_key = 1000081 * 1000193 * 1000081
+#129096451502903 = p^2 * q -> because if it's only p*q, the verification mechanism doesn't work, hence why we use p^2 * q
+#doesn't change much, becuase the key has to be factorized anyway and one of the factors is a prime number anyway
+key3 = 11362979 * 11363239 * 11362979 #not so big
 
-dec_test = ebc_decryption(enc_test,50539,50543)
-print(dec_test)
+#enc_test = ebc_encryption(int_text,key3) #43869243335189 = 32779^2 * 40829, 1338333791 = 32779 * 40829
+#print(enc_test)
 
-end_text = int_to_str(dec_test)
+#dec_test = ebc_decryption(enc_test,11362979, 11363239) #p has to be smaller than q, otherwise it won't work for some reason
+#print(dec_test)
 
-print(end_text)
+#end_text = int_to_str(dec_test, radix)
+#print(end_text)
 
-#większy radix i większy klucz, ogarnij n = p*q, aktaulnie jest n = p^2 * q
+IV = 0
+
+test1 = encryption(int_text[0]^2343827943983,key3)
+test2 = encryption(int_text[1]^test1,key3)
+test3 = encryption(int_text[2]^test2,key3)
+
+#test1 = test1 ^ 0
+#test2 = test2 ^ test1
+#test3 = test3 ^ test2
+print(test1, test2, test3)
+
+test4 = decryption(test1,11362979, 11363239)
+#test4 = test4 ^ 2343827943983
+test5 = decryption(test2,11362979, 11363239) 
+#test5 = test5 ^ test1
+test6 = decryption(test3,11362979, 11363239) 
+#test6 = test6 ^ test2
+
+print(test4, test5, test6)
+
+#cenc_test = cbc_encryption(int_text,key,IV)
+#print(cenc_test)
+
+#cdec_test = cbc_decryption(cenc_test,50539, 50543, IV)
+#print(cdec_test)
+
+
+#[1868853104, 543518326, 1864396644, 1685680994, 1970168169, 4479008]
+#[36525370897054, 39489659200212, 52889442799461, 107515953933006, 19606803028060, 20061512664064]
+
+
+#[36525370897054, 69297185668918, 2045933516730, 127811932270049, 42800105976976, 92633153595235]
+
